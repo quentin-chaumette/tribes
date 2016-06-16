0ad25fb544f5a58fa2ad16fb70db695c364f01e7
Annabelle Ruiz <annabelle.ruiz@hetic.net>
2016-06-16 03:36:28 +0200

    Finish card admin ToDo

--- views/dashboard.php
+++ views/dashboard.php
@@ -22,15 +22,16 @@
 
 		<section class="requests content-container">
 
-			<!-- CARD : ADMIN que quelqu'un a posté, qui n'est pas encore prise "A faire" ADD CLASS = toTake-->
+			<!-- CARD : ADMIN que quelqu'un a posté, qui n'est pas encore prise "À prendre" ADD CLASS = toTake-->
 			<div class="card toTake">
 				<div class="card-wrapper">
 					<!-- LEFT -->
 					<div>
 						<span class="state">À faire</span>
 						<span>il y a 2 minutes</span>
+						<span class="more"> Voir plus</span>
 						<span class="less">Voir moins</span>
-						<h4 class="content">Problème matériel d’une ampoule au rez-de-chaussée. <span class="more"> Voir plus</span></h4>
+						<h4 class="content">Problème matériel d’une ampoule au rez-de-chaussée.</h4>
 						<p>L’ampoule en face de chez moi est grillée. Le plafond est très haut, quelqu’un se rendrait disponible pour la changer ?</p>
 						<a href="http://IMG-capture-0034.jpg" download="fichier_Tribes" class="item-subtitle">IMG-capture-0034.jpg</a>
 						<div>
@@ -47,6 +48,39 @@
 							<button type="submit" value="Submit">
 								<i class="fa fa-thumbs-up" aria-hidden="true"></i>
 								Je prends
+							</button>
+							<a href="mailto:mon_syndic@syndic.net">Envoyer au Syndic</a>
+						</div>
+					</div>
+				</div>
+			</div><!--
+			--><!-- CARD : que l'admin user a choisi de faire "À faire", ADD CLASS = toDo--><div class="card toDo">
+				<div class="card-wrapper">
+					<!-- LEFT -->
+					<div>
+						<span class="state">Vous</span>
+						<span>il y a 5 heures</span>
+						<span class="more"> Voir plus</span>
+						<span class="less">Voir moins</span>
+						<h4 class="content">Problème matériel d’une ampoule au rez-de-chaussée.</h4>
+						<p>L’ampoule en face de chez moi est grillée. Le plafond est très haut, quelqu’un se rendrait disponible pour la changer ?</p>
+						<a href="http://IMG-capture-0034.jpg" download="fichier_Tribes" class="item-subtitle">IMG-capture-0034.jpg</a>
+						<div>
+							<img class="user-img" src="http://lorempixel.com/200/200/cats" alt="">
+							<div>
+								<h5 class="title">Corentin <span>Carilo</span></h5>
+								<p class="item-subtitle">Bâtiment A - 1er étage</p>
+							</div>
+						</div>
+					</div><!-- RIGHT
+					--><div>
+						<div>
+							<p class="item-subtitle">Ne laissons pas le Syndic nous facturer 5 fois plus cher !</p>
+							<button type="submit" value="Submit" class="submit">
+								<i class="fa fa-check" aria-hidden="true"></i>
+							</button>
+							<button type="submit" value="Submit" class="cancel">
+								<i class="fa fa-times" aria-hidden="true"></i>
 							</button>
 							<a href="mailto:mon_syndic@syndic.net">Envoyer au Syndic</a>
 						</div>
